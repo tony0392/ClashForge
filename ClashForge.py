d3b0c1163a1ab147cd2173cef029957447246aaa
tony0392 <54049750+tony0392@users.noreply.github.com>
2025-01-18 17:15:49 +0800

    Update ClashForge.py

--- ClashForge.py
+++ ClashForge.py
@@ -40,7 +40,7 @@ MAX_CONCURRENT_TESTS = 100
 LIMIT = 50 # 最多保留LIMIT个节点
 CONFIG_FILE = 'clash_config.yaml'
 INPUT = "input" # 从文件中加载代理节点，支持yaml/yml、txt(每条代理链接占一行)
-BAN = ["中国", "China", "CN", "电信", "移动", "联通"]
+BAN = ["中国", "China", "CN", "电信",  "联通"]
 headers = {
     'Accept-Charset': 'utf-8',
     'Accept': 'text/html,application/x-yaml,*/*',
